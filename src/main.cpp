316a2a40fbcdb0176228cd534a697c9ad5a933ac
Dániel Szoboszlay <dszoboszlay@gmail.com>
2011-11-26 13:24:29 +0100

    Test setup added and used from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "../include/common.hpp"
+#include "../include/clouddef.hpp"
+#include "../include/cloudgen.hpp"
 
 // Entry point of the application
 int main(int argc, char** argv)
@@ -6,7 +8,11 @@ int main(int argc, char** argv)
   QApplication app(argc, argv);
 
   // FIXME: this is just a demo GUI
+  CloudDef* cloudDef = CloudDef::create("priv/test1.ini");
+  CloudGen cloudGen;
   QGraphicsView widget;
+  widget.setScene(cloudGen.renderScene(*cloudDef, &widget));
+  delete cloudDef;
 
   widget.show();
   return app.exec();
